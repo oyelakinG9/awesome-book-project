2344fd861db9f71c78b250dc2ae7ca087297e9c9
oyelakinG9 <oyelakinr37@gmail.com>
2022-01-04 21:52:38 +0100

    feat: Fix liter

--- js/main.js
+++ js/main.js
@@ -1,62 +1,58 @@
 const myBookList = [];
-const submit = document.querySelector('#submit');
+const submit = document.querySelector("#submit");
 const list = document.querySelector("#bookList");
-const bookTitle = document.querySelector('#inputTitle');
-const bookAuthor = document.querySelector('#inputAuthor');
+const bookTitle = document.querySelector("#inputTitle");
+const bookAuthor = document.querySelector("#inputAuthor");
 
 class Book {
-    constructor(title, author) {
-        this.title = title,
-            this.author = author;
-    }
+  constructor(title, author) {
+    (this.title = title), (this.author = author);
+  }
 };
 
 function addBookToLibrary() {
-    if (bookTitle.value && bookAuthor.value) {
-        myBookList.push(new Book(bookTitle.value, bookAuthor.value))
-    } else {
-        alert("Please enter all information")
-    }
-    return myBookList;
+  if (bookTitle.value && bookAuthor.value) {
+    myBookList.push(new Book(bookTitle.value, bookAuthor.value));
+  } else {
+    alert("Please enter all information");
+  }
+  return myBookList;
 };
 
-//Display book:
 function displayBooks(book) {
-    const row = document.createElement('tr');
-    const createTitle = document.createElement('td');
-    const createAuthor = document.createElement('td');
-    const createRemoveBtn = document.createElement('td');
+  const row = document.createElement("tr");
+  const createTitle = document.createElement("td");
+  const createAuthor = document.createElement("td");
+  const createRemoveBtn = document.createElement("td");
 
-    createTitle.innerHTML = book.title;
-    createAuthor.innerHTML = book.author;
-    createRemoveBtn.innerHTML = ` <a class="btn-remove deleteRow">remove</a>`;
-    
-    row.appendChild(createTitle);
-    row.appendChild(createAuthor);
-    row.appendChild(createRemoveBtn);
-    list.appendChild(row);
+  createTitle.innerHTML = book.title;
+  createAuthor.innerHTML = book.author;
+  createRemoveBtn.innerHTML = `<a class="btn-remove deleteRow">remove</a>`;
 
-    createRemoveBtn.classList.add('deleteRow');
+  row.appendChild(createTitle);
+  row.appendChild(createAuthor);
+  row.appendChild(createRemoveBtn);
+  list.appendChild(row);
+
+  createRemoveBtn.classList.add("deleteRow");
 };
 
+list.addEventListener("click", function removeBook(e) {
+  if (e.target.classList.contains("deleteRow")) {
+    let eachIndex = e.target.parentElement.rowIndex - 1;
+    console.log(eachIndex);
+    e.target.parentElement.parentElement.remove();
 
-list.addEventListener('click', function removeBook(e) {
-    if (e.target.classList.contains('deleteRow')) {
-        let eachIndex = e.target.parentElement.rowIndex - 1
-        console.log(eachIndex)
-        e.target.parentElement.parentElement.remove();
-
-        myBookList.forEach((book, index) => {
-            if (index === eachIndex) {
-                myBookList.splice[eachIndex, 1]
-            }
-        });
-    }
-    return myBookList;
+    myBookList.forEach((book, index) => {
+      if (index === eachIndex) {
+        myBookList.splice[(eachIndex, 1)];
+      }
+    });
+  }
+  return myBookList;
 });
 
-
-submit.addEventListener('click', (e) => {
-    addBookToLibrary();
-    displayBooks(myBookList[myBookList.length - 1])
+submit.addEventListener("click", (e) => {
+  addBookToLibrary();
+  displayBooks(myBookList[myBookList.length - 1]);
 });
